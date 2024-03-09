7754dc524c7306202459e21eeda6dfd6c25f5ac0
Roxedus <me@roxedus.dev>
2024-03-09 03:16:02 +0000

    tf updates

--- terraform/inputs.tf
+++ terraform/inputs.tf
@@ -6,17 +6,3 @@ variable "OAUTH_CLIENT_SECRET" {
   type        = string
   description = "Secret for Tailscale"
 }
-
-variable "PROXMOX_VE_API_TOKEN" {
-  type        = string
-  description = "API Token for Proxmox"
-
-}
-variable "PROXMOX_VE_SSH_USERNAME" {
-  type        = string
-  description = "SSH Username for Proxmox hosts"
-}
-variable "PROXMOX_VE_SSH_PASSWORD" {
-  type        = string
-  description = "SSH Password for Proxmox hosts"
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,18 +4,6 @@ terraform {
       source  = "tailscale/tailscale"
       version = "0.13.11"
     }
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.40.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.0"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = "3.4.0"
-    }
   }
 }
 provider "tailscale" {
@@ -24,17 +12,3 @@ provider "tailscale" {
   oauth_client_id     = var.OAUTH_CLIENT_ID
   oauth_client_secret = var.OAUTH_CLIENT_SECRET
 }
-
-provider "proxmox" {
-  endpoint  = "https://pve.rostvik.site:8006/"
-  api_token = var.PROXMOX_VE_API_TOKEN
-  insecure  = true
-  ssh {
-    username = var.PROXMOX_VE_SSH_USERNAME
-    password = var.PROXMOX_VE_SSH_PASSWORD
-  }
-}
-
-provider "random" {}
-
-provider "http" {}
